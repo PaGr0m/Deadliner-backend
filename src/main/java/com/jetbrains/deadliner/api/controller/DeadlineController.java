1f9256cf68fb75a36f7b1074e79a25b07cbc7e5d
Pavel Gromov <pagrom27@gmail.com>
2019-10-29 14:41:37 +0300

    Routes

--- src/main/java/com/jetbrains/deadliner/api/controller/DeadlineController.java
+++ src/main/java/com/jetbrains/deadliner/api/controller/DeadlineController.java
@@ -14,7 +14,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.UUID;
 
 @RestController
-@RequestMapping("/  deadlines")
+@RequestMapping("/deadlines")
 public class DeadlineController {
 
 //    private final DeadlineMapper deadlineMapper;
